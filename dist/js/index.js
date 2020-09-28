c2fe0820b20af2add0ee13ee90608f5a5cbfb7e6
Miguel Tostado <miguel.tostado@migueltostado.com>
2020-09-28 17:25:18 -0400

    Finish layout. Working on testing form submission to emails.

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,11 +1,7 @@
 // ! Adjust the height modifier for different screen sizes
 
-const header = document.getElementById('id')
-const services = document.getElementById('services')
-const portfolio = document.getElementById('portfolio')
-const about = document.getElementById('about')
-const contact = document.getElementById('contact')
 const navLinks = document.querySelectorAll('.nav--link')
+
 /*
  Updates the navigation links to show which one is active as you scroll down the page.
  */
@@ -23,4 +19,44 @@ window.addEventListener('scroll', (event) => {
       link.classList.remove('current')
     }
   })
+})
+
+/*
+Gets current year and displays it in the footer
+*/
+const footerCopy = document.getElementsByClassName('footer--year')
+const currentYear = new Date().getFullYear()
+
+console.log(footerCopy)
+
+footerCopy[0].textContent = currentYear
+
+/*
+Send form data to email
+*/
+const nodemailer = require('nodemailer')
+const formSubmitBtn = document.getElementById('form--submit')
+console.log(formSubmitBtn)
+
+let transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'af43b9ecbef3c0',
+    pass: 'c9e78112ce2a99',
+  },
+})
+
+const message = {
+  from: 'mtostado2@gmail.com',
+  to: 'hello@migueltostado.com',
+  subject: 'web inquiry',
+  text: 'test message',
+}
+transport.sendMail(message, (err, info) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(info)
+  }
 })
